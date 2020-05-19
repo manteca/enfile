b44138e63e6614a952a13ada2c27e65835d10002
felipe <felipe.fornet@factor-a.de>
2020-05-19 15:41:26 +0200

    change some of tehg messages. Had extra sapaces

--- enfile.sh
+++ enfile.sh
@@ -31,29 +31,29 @@ encry(){
   print_terminal "Start encrytion of file $1"
 
   if [ -f $1 ]; then
-    print_terminal " File to encrypt exist" true
+    print_terminal "File to encrypt exist" true
     if [ -f $2 ]; then
-      print_terminal " Public-key does exist" true
+      print_terminal "Public-key does exist" true
       openssl rand -base64 32 > key.bin
       # openssl rand -hex 64 -out key.bin
       openssl aes-256-cbc -a -pbkdf2 -salt -in $1 -out $1.enc -k $(cat key.bin)
       openssl rsautl -encrypt -pubin -inkey <(ssh-keygen -e -f $2 -m PKCS8) -in key.bin -out key.bin.enc && rm -f key.bin
-      print_terminal " File to encrypted $1 > $1.enc" true
+      print_terminal "File to encrypted $1 > $1.enc" true
     else
-      print_terminal " Public-key does not exist" false
+      print_terminal "Public-key does not exist" false
     fi
   else
-    print_terminal " File to encrypt does not exist $1" false
+    print_terminal "File to encrypt does not exist $1" false
   fi
 }
 
 decry(){
   if [ -f $1 ]; then
-    print_terminal " File to dencrypt exist" true
+    print_terminal "File to dencrypt exist" true
     if [ -f ]; then
-      print_terminal " File to key.bin.enc exist" true
+      print_terminal "File to key.bin.enc exist" true
       if [ -f $2 ]; then
-        print_terminal " Private-key does exist" true
+        print_terminal "Private-key does exist" true
         openssl rsautl -decrypt -inkey $2 -in key.bin.enc -out key.bin && rm -f key.bin.enc
         openssl aes-256-cbc -d -a -pbkdf2 -in $1 -out ${1:0:$((length-4))} -k $(cat key.bin)
       else
